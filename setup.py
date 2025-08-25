f14c6d8d34c77bd48c273eff65d77f2dbbc80b0b
pietro_marini <pietromarini00@gmail.com>
2025-08-25 13:20:38 -0400

    setup.py update

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('requirements.txt') as f:
 
 setup(
     name='data_gatherer',
-    version='0.1.1',
+    version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.11',
 )
