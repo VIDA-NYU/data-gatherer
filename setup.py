32fcf7f42941b325bf9550ba8bfc137e6a629a3e
pietro_marini <pietromarini00@gmail.com>
2025-08-25 18:57:41 -0400

    Fix RTD build: specify Python 3.11, and read description md with setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
+import os
+
+def read_readme():
+    with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf8') as file:
+        return file.read()
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+
+long_description = read_readme()
 
 setup(
     name='data_gatherer',
@@ -10,4 +17,9 @@ setup(
     include_package_data=True,
     install_requires=requirements,
     python_requires='>=3.11',
+    description="DataGatherer Library",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/VIDA-NYU/data-gatherer',
+    keywords=['Information Extraction', 'NYU'],
 )
